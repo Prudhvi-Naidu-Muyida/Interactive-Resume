68d68c91d1ca91948a01198be3bee8816c721095
Prudhvi-Naidu-Muyida <prudhvinaidu.muyida@gmail.com>
2025-08-12 22:10:30 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import InteractiveResume from './InteractiveResume.jsx'
-import './styles.css'
-
-createRoot(document.getElementById('root')).render(<InteractiveResume />)
